9da788e73afe6c48b3c9290dfde450b030bb0e78
Maksim <maxim.zinchuk@gmail.com>
2025-07-02 15:04:03 +0300

    Refactor type declarations in shims-vue.d.ts for improved clarity and maintainability

--- src/VirtoCommerce.MarketplaceRegistrationModule.Web/vcmp-registration/src/shims-vue.d.ts
+++ src/VirtoCommerce.MarketplaceRegistrationModule.Web/vcmp-registration/src/shims-vue.d.ts
@@ -1,28 +1,19 @@
 /* eslint-disable */
-import { CoreBladeAdditionalSettings, DynamicGridSchema, DynamicDetailsSchema } from "@vc-shell/framework";
 
-import type { Component, Ref } from "vue";
-import type {
-  ComponentCustomProperties as _ComponentCustomProperties,
-} from 'vue';
+import { CoreBladeAdditionalSettings } from "@vc-shell/framework";
+import type { Ref } from "vue";
 import type { Composer } from "vue-i18n";
-import type { moment } from "moment";
-import type { Vue } from "vue";
-import type { VueRouter } from "vue-router";
-import type { VeeValidate } from "vee-validate";
-import type { VueI18n } from "vue-i18n";
-import type { VcShellFramework } from "@vc-shell/framework";
-
 
 declare module "*.vue" {
-  import type {DefineComponent} from "vue";
+  import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
 
 declare module "@vue/runtime-core" {
-  interface ComponentCustomProperties {
-    $hasAccess: (permissions: string | string[]) => boolean;
+  interface ComponentCustomProperties extends _ComponentCustomProperties {
+    $mergeLocaleMessage: Composer<{}, {}, {}, string, never, string>["mergeLocaleMessage"];
+    $hasAccess: (permissions: string | string[] | undefined) => boolean;
     $isPhone: Ref<boolean>;
     $isTablet: Ref<boolean>;
     $isMobile: Ref<boolean>;
@@ -31,19 +22,7 @@ declare module "@vue/runtime-core" {
     $t: (key: string, ...args: any[]) => string;
   }
 
-  interface ComponentOptionsBase extends CoreBladeAdditionalSettings {
-  }
-}
-
-declare global {
-  interface Window {
-    Vue: typeof vue;
-    VueRouter: typeof vueRouter;
-    VeeValidate: typeof veeValidate;
-    VueI18n: typeof vueI18n;
-    moment: moment;
-    VcShellFramework: any;
-  }
+  interface ComponentOptionsBase extends CoreBladeAdditionalSettings {}
 }
 
 export {};
